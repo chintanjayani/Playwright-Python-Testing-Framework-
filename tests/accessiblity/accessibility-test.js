f746c06111f7d448db9d303ee6370ef88b2ecf8b
chintanjayani1@gmail.com <chintanjayani1@gmail.com>
2025-01-26 23:23:35 +0000

    initial commit

--- tests/accessiblity/accessibility-test.js
+++ tests/accessiblity/accessibility-test.js
@@ -2,14 +2,15 @@ const puppeteer = require('puppeteer');
 const { AxePuppeteer } = require('@axe-core/puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true, 
+    args: ['--no-sandbox', '--disable-setuid-sandbox'], 
+  });
   const page = await browser.newPage();
 
-  // Replace with the URL you want to test
   const url = 'https://automationintesting.online';
   await page.goto(url);
 
-  // Inject Axe and run the accessibility tests
   const results = await new AxePuppeteer(page).analyze();
 
   console.log('Accessibility Violations:');
